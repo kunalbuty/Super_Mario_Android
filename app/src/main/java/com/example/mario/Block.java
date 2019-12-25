56db99e4ae4df71570fa0749b9187de1f9fe3c6b
kunalbuty <42533195+kunalbuty@users.noreply.github.com>
2019-12-25 17:46:37 +0300

    removed more duplicates

--- app/src/main/java/com/example/mario/Block.java
+++
@@ -1,9 +0,0 @@
-package com.example.mario;
-
-// Standard unbreakable block
-public class Block extends GameObject {
-    @Override
-    public void collide(GameObject other) {
-
-    }
-}
--- app/src/main/java/com/example/mario/QuestionBlock.java
+++
@@ -1,8 +0,0 @@
-package com.example.mario;
-
-// Block containing item
-public class QuestionBlock extends Block {
-    public void collide(GameObject other) {
-        // If hit by player from below, and contains item
-    }
-}
